c296b661b57e778bb51251f84003dea553c63795
Shivani Shah <shahshivani312>
2020-07-11 11:50:31 +0530

    updateed Code

--- src/app/onboard/onboard.component.ts
+++ src/app/onboard/onboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-onboard',
@@ -11,5 +12,22 @@ export class OnboardComponent implements OnInit {
 
   ngOnInit() {
   }
+  Register = new FormGroup({
+    fname: new FormControl(''),
+    lname: new FormControl(''),
+    email: new FormControl(''),
+    faname: new FormControl(''),
+    maname: new FormControl(''),
+    dob: new FormControl(''),
+    phone: new FormControl(''),
+    address: new FormControl(''),
+    pincode: new FormControl(''),
+    gender: new FormControl('')
+});
 
+
+register(){
+  console.log(this.Register);
+  
+}
 }
